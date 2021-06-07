8e13333e8f1b10d0b23dd4ad5c9d9313ae827e7d
preeti-23 <67831895+preeti-23@users.noreply.github.com>
2021-06-07 20:08:21 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+  return (
+  <div className="note">
+    <h1>Javascript and React.js</h1>
+    <p> This was a amazing bootcamp taken by Shuaurya Sinha and we covered Scratch
+      including Javascript, React.js, HTML</p>
+  </div>
+  );
+}
+
+export default Note;
